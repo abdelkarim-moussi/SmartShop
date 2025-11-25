58da32740d2a5650223b9ff6930e9a99933a8535
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-26 00:31:33 +0100

    refactor : update user entity

--- src/main/java/com/app/smartshop/infrastructure/persistence/entity/UserEntity.java
+++ src/main/java/com/app/smartshop/infrastructure/persistence/entity/UserEntity.java
@@ -1,6 +1,9 @@
 package com.app.smartshop.infrastructure.persistence.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -15,7 +18,11 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     @Column(nullable = false)
+    @NotBlank(message = "name is required")
+    @Size(max = 20,min = 3,message = "username must contain at leat 3 characters")
     private String userName;
+    @NotBlank(message = "password is required")
+    @Size(max = 20, min = 8, message = "password must contain at least 8 characters")
     @Column(nullable = false)
     private String hashedPassword;
 }
