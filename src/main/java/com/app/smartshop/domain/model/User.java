00d433540db298c71af17f6077f9068ef0dfb1c5
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-25 23:23:46 +0100

    feat : create user domain model

--- src/main/java/com/app/smartshop/domain/model/User.java
+++ src/main/java/com/app/smartshop/domain/model/User.java
@@ -1,4 +1,12 @@
 package com.app.smartshop.domain.model;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    private String userName;
+    private String email;
 }
