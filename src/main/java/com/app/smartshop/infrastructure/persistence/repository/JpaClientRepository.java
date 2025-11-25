a835c1f8ef7e3e0ecbd1411919f80a9c3c52cc3a
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-25 17:21:38 +0100

    feat : client repository implementation to map between domain model and entity safely

--- src/main/java/com/app/smartshop/infrastructure/persistence/repository/JpaClientRepository.java
+++ src/main/java/com/app/smartshop/infrastructure/persistence/repository/JpaClientRepository.java
@@ -1,8 +1,11 @@
 package com.app.smartshop.infrastructure.persistence.repository;
 
-import com.app.smartshop.domain.model.Client;
 import com.app.smartshop.domain.repository.IClientRepository;
+import com.app.smartshop.infrastructure.persistence.entity.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JPAClientRepository extends JpaRepository<Client,String>, IClientRepository {
+import java.util.Optional;
+
+public interface JpaClientRepository extends JpaRepository<ClientEntity,String> {
+    Optional<ClientEntity> findByEmail(String email);
 }
