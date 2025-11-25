b695c2330f4a95900d519510064a94aedf45da93
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-25 12:14:10 +0100

    feat: implement client creation endpoint controller

---
+++ src/main/java/com/app/smartshop/infrastructure/controller/ClientController.java
@@ -0,0 +1,24 @@
+package com.app.smartshop.infrastructure.controller;
+import com.app.smartshop.application.dto.client.ClientRequestDTO;
+import com.app.smartshop.application.dto.client.ClientResponseDTO;
+import com.app.smartshop.application.service.ClientService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clients")
+@RequiredArgsConstructor
+public class ClientController {
+    private final ClientService clientService;
+
+    @PostMapping
+    public ResponseEntity<ClientResponseDTO> createClient(@RequestBody @Valid ClientRequestDTO clientRequestDTO){
+        ClientResponseDTO clientResponseDTO = clientService.createClient(clientRequestDTO);
+        return ResponseEntity.ok(clientResponseDTO);
+    }
+}
