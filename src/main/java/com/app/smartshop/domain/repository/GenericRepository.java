5bcc197a8603f0cef719079ed0d7696a9524c648
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-25 10:15:38 +0100

    feat : implement generic repository and client repository

---
+++ src/main/java/com/app/smartshop/domain/repository/GenericRepository.java
@@ -0,0 +1,10 @@
+package com.app.smartshop.domain.repository;
+import java.util.List;
+import java.util.Optional;
+
+public interface GenericRepository<T,Id> {
+    public T save(T t);
+    public void delete(Id id);
+    public Optional<T> findById(Id id);
+    public List<T> findAll();
+}
---
+++ src/main/java/com/app/smartshop/domain/repository/IClientRepository.java
@@ -0,0 +1,7 @@
+package com.app.smartshop.domain.repository;
+
+import com.app.smartshop.domain.model.Client;
+
+public interface IClientRepository extends GenericRepository<Client,String> {
+
+}
