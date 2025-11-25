552fc3433072f847abc72ff2fc8759d40ce9f7fe
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-25 17:22:07 +0100

    feat : client repository implementation to map between domain model and entity safely

--- src/main/java/com/app/smartshop/infrastructure/persistence/repository/ClientRepositoryImpl.java
+++ src/main/java/com/app/smartshop/infrastructure/persistence/repository/ClientRepositoryImpl.java
@@ -1,4 +1,47 @@
 package com.app.smartshop.infrastructure.persistence.repository;
 
-public class ClientRepositoryImpl {
+import com.app.smartshop.domain.model.Client;
+import com.app.smartshop.domain.repository.IClientRepository;
+import com.app.smartshop.infrastructure.mapper.ClientModelEntityMapper;
+import com.app.smartshop.infrastructure.persistence.entity.ClientEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ClientRepositoryImpl implements IClientRepository {
+    private final JpaClientRepository jpaClientRepository;
+    private final ClientModelEntityMapper clientModelEntityMapper;
+
+    @Override
+    public Optional<Client> findByEmail(String email) {
+        return jpaClientRepository.findByEmail(email).map(clientModelEntityMapper::toDomainModel);
+    }
+
+    @Override
+    public Client save(Client client) {
+        ClientEntity entity = clientModelEntityMapper.toEntity(client);
+        return clientModelEntityMapper.toDomainModel(jpaClientRepository.save(entity));
+    }
+
+    @Override
+    public void delete(Client client) {
+        ClientEntity entity = clientModelEntityMapper.toEntity(client);
+        jpaClientRepository.delete(entity);
+    }
+
+    @Override
+    public Optional<Client> findById(String id) {
+        return jpaClientRepository.findById(id).map(clientModelEntityMapper::toDomainModel);
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return jpaClientRepository.findAll()
+                .stream()
+                .map(clientModelEntityMapper::toDomainModel).toList();
+    }
 }
