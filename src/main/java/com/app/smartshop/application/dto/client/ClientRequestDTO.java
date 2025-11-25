9512f7e51f5150289d2e69204d99c04aa443d280
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-25 11:48:45 +0100

    feat : client dtos for request and response

---
+++ src/main/java/com/app/smartshop/application/dto/client/ClientRequestDTO.java
@@ -0,0 +1,14 @@
+package com.app.smartshop.application.dto.client;
+
+import com.app.smartshop.domain.enums.LoyaltyLevel;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class ClientRequestDTO {
+    @NotBlank(message = "email must not be blanc or null")
+    private String name;
+    @Email(message = "email is required")
+    private String email;
+    @NotBlank
+    private LoyaltyLevel loyaltyLevel;
+}
---
+++ src/main/java/com/app/smartshop/application/dto/client/ClientResponseDTO.java
@@ -0,0 +1,10 @@
+package com.app.smartshop.application.dto.client;
+
+import com.app.smartshop.domain.enums.LoyaltyLevel;
+
+public class ClientResponseDTO {
+    private String id;
+    private String name;
+    private String email;
+    private LoyaltyLevel loyaltyLevel;
+}
