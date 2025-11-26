0a7d009b8c2bb47717642ac959215782a0740fdc
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-26 16:57:36 +0100

    feat: create authResponse dto

--- src/main/java/com/app/smartshop/application/dto/client/AuthResponse.java
+++ src/main/java/com/app/smartshop/application/dto/client/AuthResponse.java
@@ -1,4 +1,14 @@
 package com.app.smartshop.application.dto.client;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
+    @JsonProperty("JSESSIONID")
+    private String jsessionId;
 }
