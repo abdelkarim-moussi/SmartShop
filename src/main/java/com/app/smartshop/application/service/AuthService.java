704b7001650e4cfe23c123859da78b752399bdc3
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-26 10:20:24 +0100

    feat: implement login method logic, auth service

---
+++ src/main/java/com/app/smartshop/application/service/AuthService.java
@@ -0,0 +1,38 @@
+package com.app.smartshop.application.service;
+import com.app.smartshop.application.util.LoginResult;
+import com.app.smartshop.domain.model.User;
+import com.app.smartshop.domain.repository.IUserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final IUserRepository userRepository;
+
+    public LoginResult login(String userName, String password, boolean rememberMe){
+        User user = userRepository.findByUserName(userName).orElseThrow(
+                ()-> new EntityNotFoundException("there is no User with this data "+"username : "+userName+"/ password : "+password)
+        );
+
+        boolean matches = BCrypt.checkpw(password,user.getHashedPassword());
+
+        if(!matches){
+            return LoginResult.builder()
+                    .failure(true)
+                    .message("wrong password")
+                    .build();
+        }else {
+            return LoginResult.builder()
+                    .success(true)
+                    .user(user)
+                    .rememberMe(rememberMe)
+                    .build();
+        }
+
+    }
+
+}
