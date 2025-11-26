147dcd59840740a60aacf4bce4cff0225a28b1dd
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-26 16:58:05 +0100

    refactor: update login return

--- src/main/java/com/app/smartshop/infrastructure/controller/AuthController.java
+++ src/main/java/com/app/smartshop/infrastructure/controller/AuthController.java
@@ -1,10 +1,16 @@
 package com.app.smartshop.infrastructure.controller;
 
 import com.app.smartshop.application.dto.client.AuthRequest;
+import com.app.smartshop.application.dto.client.AuthResponse;
+import com.app.smartshop.application.dto.client.RegisterRequest;
 import com.app.smartshop.application.service.AuthService;
 import com.app.smartshop.application.util.LoginResult;
 import com.app.smartshop.application.util.SessionManager;
+import com.app.smartshop.domain.enums.UserRole;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +25,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterRequest registerRequest){
+
+        String success = authService.register(registerRequest.getUserName(),registerRequest.getPassword(), registerRequest.getRole());
+
+        if(success.isEmpty()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        return ResponseEntity.ok(success);
+
+    }
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthRequest authRequest,
                                 HttpServletRequest request){
@@ -30,7 +49,7 @@ public class AuthController {
         }
 
         String cookieId = SessionManager.createSession(request,result);
-        return ResponseEntity.ok(cookieId);
+        return ResponseEntity.ok().body(cookieId);
 
     }
 }
