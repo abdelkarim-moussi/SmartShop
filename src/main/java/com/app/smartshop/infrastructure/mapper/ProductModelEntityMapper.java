bfb2ef2c282e66fdf937f25edf3b5dc5bf2b8fcd
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-27 17:03:59 +0100

    feat:product mapper

--- src/main/java/com/app/smartshop/infrastructure/mapper/ProductModelEntityMapper.java
+++ src/main/java/com/app/smartshop/infrastructure/mapper/ProductModelEntityMapper.java
@@ -1,4 +1,11 @@
 package com.app.smartshop.infrastructure.mapper;
 
-public class ProductModelEntityMapper {
+import com.app.smartshop.domain.model.Product;
+import com.app.smartshop.infrastructure.persistence.entity.ProductEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ProductModelEntityMapper {
+    Product toModel(ProductEntity entity);
+    ProductEntity toEntity(Product product);
 }
