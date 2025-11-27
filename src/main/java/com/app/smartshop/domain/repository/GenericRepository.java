b6b2350aec35919d475731570e4289815ea44e5d
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-27 14:36:38 +0100

    feat: add update method

--- src/main/java/com/app/smartshop/domain/repository/GenericRepository.java
+++ src/main/java/com/app/smartshop/domain/repository/GenericRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 public interface GenericRepository<T,ID> {
     public T save(T t);
+    public T update(T t);
     public void delete(T t);
     public Optional<T> findById(ID id);
     public Page<T> findAll(DomainPageRequest domainPageRequest, Filters filters);
