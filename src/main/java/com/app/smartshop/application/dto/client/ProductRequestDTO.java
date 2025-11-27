b2186acaa94e2e06b1d2df8b19500acf4d87844a
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-27 16:58:50 +0100

    feat: create product request class

---
+++ src/main/java/com/app/smartshop/application/dto/client/ProductRequestDTO.java
@@ -0,0 +1,20 @@
+package com.app.smartshop.application.dto.client;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductRequestDTO {
+    @NotBlank(message = "product name is required")
+    private String name;
+    @Min(1)
+    private BigDecimal unitPrice;
+    @Min(1)
+    private int stock;
+}
