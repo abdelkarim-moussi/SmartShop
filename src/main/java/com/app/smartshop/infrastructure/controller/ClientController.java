fb56719ab49d854e1608a5d642e3d338165a0b1b
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-27 12:23:32 +0100

    feat: implement getAllClient endpoitn in client controller

--- src/main/java/com/app/smartshop/infrastructure/controller/ClientController.java
+++ src/main/java/com/app/smartshop/infrastructure/controller/ClientController.java
@@ -1,11 +1,12 @@
 package com.app.smartshop.infrastructure.controller;
 import com.app.smartshop.application.dto.client.ClientRequestDTO;
 import com.app.smartshop.application.dto.client.ClientResponseDTO;
-import com.app.smartshop.application.service.ClientServiceImpl;
+import com.app.smartshop.application.dto.client.Filters;
 import com.app.smartshop.application.service.IClientService;
+import com.app.smartshop.domain.repository.specification.DomainPageRequest;
+import com.app.smartshop.domain.repository.specification.Page;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,9 +22,36 @@ public class ClientController {
         return ResponseEntity.ok(clientResponseDTO);
     }
 
-    @GetMapping
+    @GetMapping(params = "id")
     public ResponseEntity<ClientResponseDTO> getClientById(@RequestParam(value = "id") String id){
         return ResponseEntity.
                 ok(clientService.findClientById(id));
+    }
+    @GetMapping
+    public ResponseEntity<Page<ClientResponseDTO>> getAllClient(
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "size", defaultValue = "5") Integer size,
+            @RequestParam(name = "sortBy", defaultValue = "name") String sortBy,
+            @RequestParam(name = "sortDir", defaultValue = "DESC") String sortDir,
+            @RequestParam(name = "loyaltyLevel", required = false) String loyaltyLevel,
+            @RequestParam(name = "search", required = false) String search
+    ){
+
+        Filters filters = Filters.builder()
+                .loyaltyLevel(loyaltyLevel)
+                .search(search)
+                .build();
+
+        DomainPageRequest domainPageRequest = DomainPageRequest.builder()
+                .page(page)
+                .size(size)
+                .sortBy(sortBy)
+                .sortDir(sortDir)
+                .build();
+
+        Page<ClientResponseDTO> page1 = clientService.findAllClients(domainPageRequest, filters);
+
+        return ResponseEntity.
+                ok(page1);
     }
 }
