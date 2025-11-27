dc58c18dde0242338b6a21008d57f939e43ea85a
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-27 14:33:28 +0100

    feat: handle DataNotExistException exception in global exception handeller

--- src/main/java/com/app/smartshop/infrastructure/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/smartshop/infrastructure/exception/GlobalExceptionHandler.java
@@ -1,4 +1,23 @@
 package com.app.smartshop.infrastructure.exception;
 
+import com.app.smartshop.application.exception.DataNotExistException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataNotExistException.class)
+    public ResponseEntity<?> handleDataNotExistException(DataNotExistException exception, WebRequest request){
+        ExceptionResponse response = ExceptionResponse.builder()
+                .status(404)
+                .error("data not found")
+                .message(exception.getMessage())
+                .path(request.getDescription(true))
+                .build();
+
+        return ResponseEntity.status(404).body(response);
+    }
 }
