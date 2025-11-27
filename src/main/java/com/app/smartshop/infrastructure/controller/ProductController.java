0e1db7ef6911c19d6ce6340ae916a71df5c486e6
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-27 17:04:32 +0100

    feat: product controller, create product endpoint

--- src/main/java/com/app/smartshop/infrastructure/controller/ProductController.java
+++ src/main/java/com/app/smartshop/infrastructure/controller/ProductController.java
@@ -1,4 +1,22 @@
 package com.app.smartshop.infrastructure.controller;
 
+import com.app.smartshop.application.dto.client.ProductRequestDTO;
+import com.app.smartshop.application.dto.client.ProductResponseDTO;
+import com.app.smartshop.application.service.IProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
+    private final IProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDTO> createNewProduct(@RequestBody @Valid ProductRequestDTO product){
+        ProductResponseDTO savedProduct = productService.createProduct(product);
+        return ResponseEntity.ok(savedProduct);
+    }
 }
