739b58ce08861218f861b63f7f629264e8cff206
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-27 14:41:05 +0100

    feat: handle EmailAlredyExist exception in global exception handler

--- src/main/java/com/app/smartshop/infrastructure/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/smartshop/infrastructure/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.app.smartshop.infrastructure.exception;
 
 import com.app.smartshop.application.exception.DataNotExistException;
+import com.app.smartshop.application.exception.EmailAleadyUsedException;
+import jakarta.validation.constraints.Email;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,7 +12,7 @@ import org.springframework.web.context.request.WebRequest;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataNotExistException.class)
-    public ResponseEntity<?> handleDataNotExistException(DataNotExistException exception, WebRequest request){
+    public ResponseEntity<ExceptionResponse> handleDataNotExistException(DataNotExistException exception, WebRequest request){
         ExceptionResponse response = ExceptionResponse.builder()
                 .status(404)
                 .error("data not found")
@@ -19,5 +21,17 @@ public class GlobalExceptionHandler {
                 .build();
 
         return ResponseEntity.status(404).body(response);
+    }
+
+    @ExceptionHandler(EmailAleadyUsedException.class)
+    public ResponseEntity<ExceptionResponse> handleEmailAlreadyExistException(EmailAleadyUsedException exception, WebRequest request){
+        ExceptionResponse response = ExceptionResponse.builder()
+                .status(409)
+                .error("email already exist")
+                .message(exception.getMessage())
+                .path(request.getDescription(true))
+                .build();
+
+        return ResponseEntity.status(409).body(response);
     }
 }
