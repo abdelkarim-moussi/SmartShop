0f9ab421f2c9d5447d9b34c211a7965a9b90e3fb
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-27 17:04:54 +0100

    feat: jpa product repository

--- src/main/java/com/app/smartshop/infrastructure/persistence/repository/JpaProductRepository.java
+++ src/main/java/com/app/smartshop/infrastructure/persistence/repository/JpaProductRepository.java
@@ -1,4 +1,11 @@
 package com.app.smartshop.infrastructure.persistence.repository;
 
-public interface JpaProductRepository {
+import com.app.smartshop.infrastructure.persistence.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JpaProductRepository extends JpaRepository<ProductEntity,String> {
+    Optional<ProductEntity> findByName(String name);
+    boolean existsByName(String name);
 }
