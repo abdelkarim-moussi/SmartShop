b3603d12579a9cc5b4c78d2f5dd991c74b0fa444
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-28 14:46:22 +0100

    feat:create order service interface

--- src/main/java/com/app/smartshop/application/service/IOrderService.java
+++ src/main/java/com/app/smartshop/application/service/IOrderService.java
@@ -1,4 +1,13 @@
 package com.app.smartshop.application.service;
+import com.app.smartshop.domain.model.Order;
+import com.app.smartshop.domain.model.search.OrderCriteria;
+import com.app.smartshop.infrastructure.controller.dto.DomainPageRequest;
+import com.app.smartshop.infrastructure.controller.dto.Page;
 
 public interface IOrderService {
+    Order createOrder(Order order);
+    Order updateOrder(String id,Order order);
+    Order findOrderById(String id);
+    void deleteOrderById(String id);
+    Page<Order> findAllOrders(DomainPageRequest domainPageRequest, OrderCriteria filters);
 }
