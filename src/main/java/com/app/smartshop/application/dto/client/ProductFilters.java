874a16898ccd48bd8c21f1ed2a01c4d983afd110
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-28 10:26:36 +0100

    refactor: fix naming issues

--- src/main/java/com/app/smartshop/application/dto/client/ProductFilters.java
+++ src/main/java/com/app/smartshop/application/dto/client/ProductFilters.java
@@ -1,4 +1,13 @@
 package com.app.smartshop.application.dto.client;
 
+import lombok.Builder;
+import lombok.Value;
+
+import java.math.BigDecimal;
+
+@Value
+@Builder
 public class ProductFilters {
+    String name;
+    BigDecimal unitPrice;
 }
