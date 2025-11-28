91280c370871cba35372237b77ec3bf5043b3a73
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-28 14:44:12 +0100

    feat: create order model that represent the command

--- src/main/java/com/app/smartshop/domain/model/Order.java
+++ src/main/java/com/app/smartshop/domain/model/Order.java
@@ -1,4 +1,27 @@
 package com.app.smartshop.domain.model;
 
+import com.app.smartshop.domain.enums.OrderStatus;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
+    private String id;
+    private LocalDateTime date;
+    private BigDecimal subtotal;
+    private BigDecimal discount;
+    private BigDecimal tva;
+    private BigDecimal total;
+    private String promotionCode;
+    private OrderStatus status;
+    private BigDecimal restAmount;
+    private Client client;
+    private List<OrderItem> articlesList;
 }
