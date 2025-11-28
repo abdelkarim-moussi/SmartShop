d4f7fc78aaa223e11860f445649ba689733d35f9
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-28 10:28:44 +0100

    feat: create existById method to verify if a product exist

--- src/main/java/com/app/smartshop/domain/repository/IProductRepository.java
+++ src/main/java/com/app/smartshop/domain/repository/IProductRepository.java
@@ -1,7 +1,8 @@
 package com.app.smartshop.domain.repository;
+import com.app.smartshop.application.dto.client.ProductFilters;
 import com.app.smartshop.domain.model.Product;
 
-public interface IProductRepository extends GenericRepository<Product,String>{
+public interface IProductRepository extends GenericRepository<Product,String, ProductFilters>{
     Product findProductByName(String name);
     boolean existsByName(String name);
     boolean existsById(String id);
