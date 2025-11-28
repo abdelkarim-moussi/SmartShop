70bfc9777fa8c71c68cfc7ddf52a27013be71128
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-28 16:41:18 +0100

    feat: implemnt the methods to calculate discount based on loyalty and promotion code, get the discount percentage based on client loyalty, calculate total discount

--- src/main/java/com/app/smartshop/domain/model/Order.java
+++ src/main/java/com/app/smartshop/domain/model/Order.java
@@ -4,9 +4,9 @@ import com.app.smartshop.domain.enums.OrderStatus;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -32,4 +32,30 @@ public class Order {
                 .reduce(BigDecimal.ZERO,BigDecimal::add);
     }
 
+    private int percentDiscountBasedOnLoyalty(){
+        return switch(this.client.getLoyaltyLevel()){
+            case BASIC -> 0;
+            case SILVER -> subtotal.compareTo(BigDecimal.valueOf(500)) > 0 ? 5 : 0;
+            case GOLD -> subtotal.compareTo(BigDecimal.valueOf(800)) > 0 ? 10 : 0;
+            case PLATINUM -> subtotal.compareTo(BigDecimal.valueOf(1200)) > 0 ? 15 : 0;
+        };
+    }
+
+    private BigDecimal calculateDiscount(int discountPercent){
+        if(discountPercent <= 0){
+            return BigDecimal.ZERO;
+        }
+        return subtotal.multiply(BigDecimal.valueOf(discountPercent)).
+                divide(BigDecimal.valueOf(100),2, RoundingMode.HALF_UP);
+    };
+
+
+    public BigDecimal calculateTotalDiscount(int i){
+        BigDecimal total = calculateDiscount(this.percentDiscountBasedOnLoyalty());
+        if(promotionCode != null && !promotionCode.isEmpty()){
+            BigDecimal promotionDiscount = calculateTotalDiscount(5);
+            total = total.add(promotionDiscount);
+        }
+        return total;
+    }
 }
