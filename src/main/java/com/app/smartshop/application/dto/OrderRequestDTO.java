883fd393851541a33d8ded15d645e01203b5a3b3
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-12-04 18:43:02 +0100

    feat: create order request dto

---
+++ src/main/java/com/app/smartshop/application/dto/OrderRequestDTO.java
@@ -0,0 +1,19 @@
+package com.app.smartshop.application.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class OrderRequestDTO {
+    @NotBlank(message = "Client ID is required")
+    private String clientId;
+    @NotNull
+    private List<OrderItemRequestDTO> itemsList;
+    private String promotionCode;
+}
