2cf959c4f8035f18568e1da0b82a28a8479f0150
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-12-07 23:04:10 +0100

    feat: create payment entity

---
+++ src/main/java/com/app/smartshop/domain/entity/Payment.java
@@ -0,0 +1,31 @@
+package com.app.smartshop.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column(nullable = false)
+    private String orderId;
+    private byte paymentNumber;
+    private BigDecimal amount;
+    private LocalDate paymentDate;
+    private LocalDate collectionDate;
+    private LocalDate dueDate;
+    private String reference;
+    private String bankName;
+    private String receiptNumber;
+}
