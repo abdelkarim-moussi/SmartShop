91a0a8a74b24ab7906b7bdc4ae7fa46a2f8eb2aa
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-12-07 21:12:33 +0100

    feat:create product increment method

---
+++ src/main/java/com/app/smartshop/domain/entity/Product.java
@@ -0,0 +1,35 @@
+package com.app.smartshop.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column(name = "name",nullable = false)
+    private String name;
+    @Column(name = "unit_price",nullable = false)
+    private BigDecimal unitPrice;
+    @Column(name="stock",nullable = false)
+    private int stock;
+    @OneToMany(mappedBy = "product")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    public void decrementStock(int demandedQuantity){
+        if(demandedQuantity > 0){
+            this.stock -= demandedQuantity;
+        }
+    }
+}
