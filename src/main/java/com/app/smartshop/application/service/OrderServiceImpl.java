5fba94dec0ed0a162f745607576d65771d22b800
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-12-07 21:12:01 +0100

    refactor: update create order method, product stock validation and incrementation

--- src/main/java/com/app/smartshop/application/service/OrderServiceImpl.java
+++ src/main/java/com/app/smartshop/application/service/OrderServiceImpl.java
@@ -1,26 +1,25 @@
 package com.app.smartshop.application.service;
 
-import com.app.smartshop.application.dto.OrderRequestDTO;
-import com.app.smartshop.application.dto.OrderResponseDTO;
+import com.app.smartshop.application.dto.*;
+import com.app.smartshop.application.exception.BusinessRuleException;
 import com.app.smartshop.application.exception.DataNotExistException;
 import com.app.smartshop.application.exception.InvalidParameterException;
 import com.app.smartshop.application.mapper.OrderModelDTOMapper;
-import com.app.smartshop.domain.model.Client;
-import com.app.smartshop.domain.model.Order;
-import com.app.smartshop.domain.model.OrderItem;
-import com.app.smartshop.domain.model.Product;
-import com.app.smartshop.domain.model.search.OrderCriteria;
-import com.app.smartshop.application.dto.DomainPageRequest;
-import com.app.smartshop.application.dto.Page;
-import com.app.smartshop.domain.repository.IClientRepository;
-import com.app.smartshop.domain.repository.IOrderRepository;
-import com.app.smartshop.domain.repository.IProductRepository;
+import com.app.smartshop.domain.enums.OrderStatus;
+import com.app.smartshop.domain.entity.Client;
+import com.app.smartshop.domain.entity.Order;
+import com.app.smartshop.domain.entity.OrderItem;
+import com.app.smartshop.domain.entity.Product;
+import com.app.smartshop.domain.entity.search.OrderCriteria;
+import com.app.smartshop.domain.repository.JpaClientRepository;
+import com.app.smartshop.domain.repository.JpaOrderRepository;
+import com.app.smartshop.domain.repository.JpaProductRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,9 +27,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderServiceImpl implements IOrderService{
 
-    private final IClientRepository clientRepository;
-    private final IProductRepository productRepository;
-    private final IOrderRepository orderRepository;
+    private final JpaClientRepository clientRepository;
+    private final JpaProductRepository productRepository;
+    private final JpaOrderRepository orderRepository;
     private final OrderModelDTOMapper mapper;
 
     @Override
@@ -41,28 +40,65 @@ public class OrderServiceImpl implements IOrderService{
                 () -> new DataNotExistException("there no client with this ID: "+order.getClientId())
         );
 
-        List<OrderItem> items = order.getItemsList()
-                .stream().map(item -> {
-                    Product product = productRepository.findById(item.getProductId())
-                            .orElseThrow(()-> new DataNotExistException("no product exist with this ID: "+item.getProductId()));
-
-                    return OrderItem.builder()
-                            .product(product)
-                            .unitPrice(product.getUnitPrice())
-                            .quantity(item.getQuantity())
-                            .build();
-                }).toList();
-
         Order newOrder = Order.builder()
                 .date(LocalDateTime.now())
                 .client(client)
                 .promotionCode(order.getPromotionCode())
-                .itemsList(items)
+                .status(OrderStatus.PENDING)
                 .build();
+
+        List<OrderItem> items = mapAndProcessOrderItems(order.getItemsList(),newOrder);
+        newOrder.setItemsList(items);
+        newOrder.calculateAmounts();
+
+        if(newOrder.getStatus().equals(OrderStatus.REJECTED)){
+            orderRepository.save(newOrder);
+            throw new BusinessRuleException("order is rejected due to insuffisant stock");
+        }
+        updateProductStock(newOrder);
 
         Order savedOrder = orderRepository.save(newOrder);
 
         return mapper.toResponseDto(savedOrder);
+    }
+
+    private List<OrderItem> mapAndProcessOrderItems(List<OrderItemRequestDTO> items, Order order){
+        return items.stream().map(item -> {
+                    Product product = productRepository.findById(item.getProductId())
+                            .orElseThrow(()-> new DataNotExistException("no product exist with this ID: "+item.getProductId()));
+
+                    OrderItem orderItem = OrderItem.builder()
+                            .product(product)
+                            .unitPrice(product.getUnitPrice())
+                            .quantity(item.getQuantity())
+                            .build();
+                    orderItem.calculateTotalLine();
+                    order.addItem(orderItem);
+
+                    if(!isStockValid(product.getId(), item.getQuantity())){
+                        order.setStatus(OrderStatus.REJECTED);
+                    }
+                    return orderItem;
+
+                }).toList();
+    }
+
+    private boolean isStockValid(String productId,int demandedQuantity){
+        Product product = productRepository.findById(productId)
+                .orElseThrow(()-> new DataNotExistException("no product exist with this ID: "+productId));
+
+        return product.getStock() >= demandedQuantity;
+    }
+
+    private void updateProductStock(Order order){
+        for (OrderItem item : order.getItemsList()){
+            Product product = item.getProduct();
+
+            int quantity = item.getQuantity();
+
+            product.decrementStock(quantity);
+            productRepository.save(product);
+        }
     }
 
     @Override
