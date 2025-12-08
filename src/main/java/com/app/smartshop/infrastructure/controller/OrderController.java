cc6087942a8c84ef7ea500029f9c9b20cb7e4801
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-12-08 02:51:41 +0100

    feat: implement order pay, confirm methods

--- src/main/java/com/app/smartshop/infrastructure/controller/OrderController.java
+++ src/main/java/com/app/smartshop/infrastructure/controller/OrderController.java
@@ -2,24 +2,38 @@ package com.app.smartshop.infrastructure.controller;
 
 import com.app.smartshop.application.dto.OrderRequestDTO;
 import com.app.smartshop.application.dto.OrderResponseDTO;
+import com.app.smartshop.application.dto.PaymentRequestDTO;
+import com.app.smartshop.application.dto.PaymentResponseDTO;
 import com.app.smartshop.application.service.IOrderService;
+import com.app.smartshop.application.service.IPaymentService;
+import com.app.smartshop.application.service.PayOrder;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 public class OrderController {
     private final IOrderService orderService;
+    private final PayOrder payOrder;
 
     @PostMapping
     public ResponseEntity<OrderResponseDTO> createNewOrder(@RequestBody @Valid OrderRequestDTO request){
         OrderResponseDTO response = orderService.createOrder(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/pay")
+    public ResponseEntity<PaymentResponseDTO> payOrder(@RequestBody @Valid PaymentRequestDTO request){
+        PaymentResponseDTO response = payOrder.payOrder(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/confirm")
+    private ResponseEntity<OrderResponseDTO> confirmOrder(@RequestParam (value = "id") String id){
+        OrderResponseDTO response = orderService.confirmOrder(id);
         return ResponseEntity.ok(response);
     }
 }
