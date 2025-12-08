e738e96deba8bebb567a68f7cb17d9503af3ea4b
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-12-08 09:06:31 +0100

    feat: create payment mapper

--- src/main/java/com/app/smartshop/application/mapper/PaymentMapper.java
+++ src/main/java/com/app/smartshop/application/mapper/PaymentMapper.java
@@ -1,4 +1,12 @@
 package com.app.smartshop.application.mapper;
 
+import com.app.smartshop.application.dto.PaymentRequestDTO;
+import com.app.smartshop.application.dto.PaymentResponseDTO;
+import com.app.smartshop.domain.entity.Payment;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface PaymentMapper {
+    PaymentResponseDTO toDTO(Payment payment);
+    Payment toEntity(PaymentRequestDTO requestDTO);
 }
